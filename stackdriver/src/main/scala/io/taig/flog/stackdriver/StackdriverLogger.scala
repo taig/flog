5576cb959d06bb5eb20b58c0b87d42de5242b886
Niklas Klein <mail@taig.io>
2019-09-12 12:49:18 +0200

    Add stacktrace to message in stackdriver

--- stackdriver/src/main/scala/io/taig/flog/stackdriver/StackdriverLogger.scala
+++ stackdriver/src/main/scala/io/taig/flog/stackdriver/StackdriverLogger.scala
@@ -4,7 +4,8 @@ import cats.effect.Sync
 import cats.implicits._
 import com.google.cloud.logging.Logging.WriteOption
 import com.google.cloud.logging.Payload.StringPayload
-import com.google.cloud.logging._
+import com.google.cloud.logging.{Option => _, _}
+import io.taig.flog.internal.Helpers
 import io.taig.flog.{Event, Level, Logger}
 
 import scala.jdk.CollectionConverters._
@@ -18,7 +19,7 @@ final class StackdriverLogger[F[_]](
   override def apply(events: List[Event]): F[Unit] = {
     val entries = events.map { event =>
       val builder = LogEntry
-        .newBuilder(StringPayload.of(event.message.value))
+        .newBuilder(payload(event).map(StringPayload.of).orNull)
         .setSeverity(severity(event))
         .setLogName(name(event))
         .setLabels(event.payload.value.asJava)
@@ -29,6 +30,15 @@ final class StackdriverLogger[F[_]](
 
     F.delay(logging.write(entries.asJava, write: _*))
   }
+
+  def payload(event: Event): Option[String] =
+    (Some(event.message.value).filter(_.nonEmpty), event.throwable) match {
+      case (Some(message), Some(throwable)) =>
+        Some(message + "\n" + Helpers.print(throwable))
+      case (message @ Some(_), None) => message
+      case (None, Some(throwable))   => Some(Helpers.print(throwable))
+      case (None, None)              => None
+    }
 
   def name(event: Event): String = event.scope.show.replace("/", "%2F")
 
