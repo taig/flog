5908ab0ee3f08b48ac1ad2de442abb1555435f7d
Niklas Klein <mail@taig.io>
2019-09-19 17:50:03 +0200

    Fix 2.12 compile errors

--- core/src/main/scala/io/taig/flog/Tracer.scala
+++ core/src/main/scala/io/taig/flog/Tracer.scala
@@ -1,7 +1,5 @@
 package io.taig.flog
 
-import java.util.UUID
-
 import cats.effect.Sync
 import cats.implicits._
 import io.taig.flog.internal.UUIDs
@@ -11,24 +9,27 @@ abstract class Tracer[F[_]] { self =>
 }
 
 object Tracer {
-  def apply[F[+_]: Sync, B](
-      logger: Logger[F]
-  )(f: (Logger[F], UUID, Throwable) => F[Nothing]): Tracer[F] = new Tracer[F] {
-    override def run[A](g: Logger[F] => F[A]): F[A] =
-      UUIDs.random[F].flatMap { trace =>
-        val tracer = logger.tracer(trace)
-        g(tracer).handleErrorWith(f(tracer, trace, _))
-      }
-  }
-
-  def reporting[F[+_]: Sync](logger: Logger[F]): Tracer[F] =
-    Tracer(logger) { (tracer, _, throwable) =>
-      tracer.failure(throwable = throwable.some) *> throwable
-        .raiseError[F, Nothing]
+  def reporting[F[_]: Sync](logger: Logger[F]): Tracer[F] =
+    new Tracer[F] {
+      override def run[A](f: Logger[F] => F[A]): F[A] =
+        UUIDs.random[F].flatMap { trace =>
+          val tracer = logger.tracer(trace)
+          f(tracer).handleErrorWith { throwable =>
+            tracer.failure(throwable = throwable.some) *> throwable
+              .raiseError[F, A]
+          }
+        }
     }
 
-  def adapting[F[+_]: Sync](logger: Logger[F]): Tracer[F] =
-    Tracer(logger) { (tracer, trace, throwable) =>
-      TracedFailure(tracer, trace, throwable).raiseError[F, Nothing]
+  def adapting[F[_]: Sync](logger: Logger[F]): Tracer[F] =
+    new Tracer[F] {
+      override def run[A](f: Logger[F] => F[A]): F[A] =
+        UUIDs.random[F].flatMap { trace =>
+          val tracer = logger.tracer(trace)
+          f(tracer).adaptErr {
+            case throwable =>
+              TracedFailure(tracer, trace, throwable)
+          }
+        }
     }
 }
