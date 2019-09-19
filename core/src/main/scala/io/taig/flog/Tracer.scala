6624af4f6dc7bdf439b15a8a32e3328be954f9a1
Niklas Klein <mail@taig.io>
2019-09-19 14:45:26 +0200

    Add Tracer.edit

--- core/src/main/scala/io/taig/flog/Tracer.scala
+++ core/src/main/scala/io/taig/flog/Tracer.scala
@@ -4,8 +4,13 @@ import cats.effect.Sync
 import cats.implicits._
 import io.taig.flog.internal.UUIDs
 
-abstract class Tracer[F[_]] {
+abstract class Tracer[F[_]] { self =>
   def run[A](f: Logger[F] => F[A]): F[A]
+
+  final def edit(f: Logger[F] => Logger[F]): Tracer[F] =
+    new Tracer[F] {
+      override def run[A](g: Logger[F] => F[A]): F[A] = self.run(f andThen g)
+    }
 }
 
 object Tracer {
