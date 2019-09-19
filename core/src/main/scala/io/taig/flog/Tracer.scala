e19a2538af248b1ea7f1d4b3289fc5f55abc7d62
Niklas Klein <mail@taig.io>
2019-09-19 13:59:23 +0200

    Simplify Tracer

--- core/src/main/scala/io/taig/flog/Tracer.scala
+++ core/src/main/scala/io/taig/flog/Tracer.scala
@@ -1,31 +1,23 @@
 package io.taig.flog
 
-import java.util.UUID
-
 import cats.effect.Sync
 import cats.implicits._
 import io.taig.flog.internal.UUIDs
 
-final class Tracer[F[_], A](logger: Logger[F], f: Logger[F] => F[A])(
-    implicit F: Sync[F]
-) {
-  val release: F[A] = UUIDs.random[F].flatMap(release(_))
-
-  def release(trace: UUID): F[A] =
-    release(logger.tracer(trace))
-
-  def release(logger: Logger[F]): F[A] =
-    f(logger).handleErrorWith { throwable =>
-      logger.failure(throwable = throwable.some) *> F.raiseError(throwable)
-    }
-
-  def evalMap[B](f: A => F[B]): Tracer[F, B] =
-    Tracer(logger)(release(_).flatMap(f))
+abstract class Tracer[F[_]] {
+  def run[A](f: Logger[F] => F[A]): F[A]
 }
 
 object Tracer {
-  def apply[F[_]: Sync, A](
-      logger: Logger[F]
-  )(f: Logger[F] => F[A]): Tracer[F, A] =
-    new Tracer[F, A](logger, f)
+  def apply[F[_]: Sync](logger: Logger[F]): Tracer[F] = new Tracer[F] {
+    override def run[A](f: Logger[F] => F[A]): F[A] =
+      UUIDs.random[F].flatMap { trace =>
+        val tracer = logger.tracer(trace)
+
+        f(tracer).handleErrorWith { throwable =>
+          tracer.failure(throwable = throwable.some) *>
+            throwable.raiseError[F, A]
+        }
+      }
+  }
 }
