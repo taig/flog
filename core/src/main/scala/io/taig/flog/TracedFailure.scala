f1327ffd8ea3f206533de52c8002c09536f06b4d
Niklas Klein <mail@taig.io>
2019-09-19 17:06:54 +0200

    Introduced TracedFailure as an alternative Tracer error handling mode

---
+++ core/src/main/scala/io/taig/flog/TracedFailure.scala
@@ -0,0 +1,7 @@
+package io.taig.flog
+
+import java.util.UUID
+import cats.implicits._
+
+final case class TracedFailure(trace: UUID, cause: Throwable)
+    extends Exception(show"Traced failure with if $trace", cause)
--- core/src/main/scala/io/taig/flog/Tracer.scala
+++ core/src/main/scala/io/taig/flog/Tracer.scala
@@ -14,7 +14,7 @@ abstract class Tracer[F[_]] { self =>
 }
 
 object Tracer {
-  def apply[F[_]: Sync](logger: Logger[F]): Tracer[F] = new Tracer[F] {
+  def reporting[F[_]: Sync](logger: Logger[F]): Tracer[F] = new Tracer[F] {
     override def run[A](f: Logger[F] => F[A]): F[A] =
       UUIDs.random[F].flatMap { trace =>
         val tracer = logger.tracer(trace)
@@ -22,6 +22,15 @@ object Tracer {
         f(tracer).handleErrorWith { throwable =>
           tracer.failure(throwable = throwable.some) *>
             throwable.raiseError[F, A]
+        }
+      }
+  }
+
+  def adapting[F[_]: Sync](logger: Logger[F]): Tracer[F] = new Tracer[F] {
+    override def run[A](f: Logger[F] => F[A]): F[A] =
+      UUIDs.random[F].flatMap { trace =>
+        f(logger.tracer(trace)).adaptErr {
+          case throwable => TracedFailure(trace, throwable)
         }
       }
   }
