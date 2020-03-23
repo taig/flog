4a4f383dca85b5a3c0d80a51d00f29420305f1dc
Niklas Klein <mail@taig.io>
2020-03-23 09:25:15 +0100

    Increase Logging API surface to simplify usage

--- modules/core/src/main/scala/io/taig/flog/algebra/Logger.scala
+++ modules/core/src/main/scala/io/taig/flog/algebra/Logger.scala
@@ -29,32 +29,76 @@ abstract class Logger[F[_]] {
   }
 
   final def debug(
-      scope: Scope = Scope.Root,
-      message: String = "",
-      payload: => Json = Json.Null,
-      throwable: Option[Throwable] = None
+      scope: Scope,
+      message: String,
+      payload: => Json,
+      throwable: Option[Throwable]
   ): F[Unit] = apply(Level.Debug, scope, message, payload, throwable)
 
+  final def debug(message: String): F[Unit] = debug(Scope.Root, message, Json.Null, None)
+  final def debug(scope: Scope, message: String): F[Unit] = debug(scope, message, Json.Null, None)
+  final def debug(payload: => Json): F[Unit] = debug(Scope.Root, "", payload, None)
+  final def debug(scope: Scope, payload: => Json): F[Unit] = debug(scope, "", payload, None)
+  final def debug(message: String, payload: => Json): F[Unit] = debug(Scope.Root, message, payload, None)
+  final def debug(scope: Scope, message: String, payload: => Json): F[Unit] = debug(scope, message, payload, None)
+  final def debug(message: String, throwable: Throwable): F[Unit] = debug(Scope.Root, message, Json.Null, Some(throwable))
+  final def debug(scope: Scope, message: String, throwable: Throwable): F[Unit] = debug(scope, message, Json.Null, Some(throwable))
+  final def debug(message: String, payload: => Json, throwable: Throwable): F[Unit] = debug(Scope.Root, message, payload, Some(throwable))
+  final def debug(scope: Scope, message: String, payload: => Json, throwable: Throwable): F[Unit] = debug(scope, message, payload, Some(throwable))
+
   final def error(
-      scope: Scope = Scope.Root,
-      message: String = "",
-      payload: => Json = Json.Null,
-      throwable: Option[Throwable] = None
+      scope: Scope,
+      message: String,
+      payload: => Json,
+      throwable: Option[Throwable]
   ): F[Unit] = apply(Level.Error, scope, message, payload, throwable)
 
+  final def error(message: String): F[Unit] = error(Scope.Root, message, Json.Null, None)
+  final def error(scope: Scope, message: String): F[Unit] = error(scope, message, Json.Null, None)
+  final def error(payload: => Json): F[Unit] = error(Scope.Root, "", payload, None)
+  final def error(scope: Scope, payload: => Json): F[Unit] = error(scope, "", payload, None)
+  final def error(message: String, payload: => Json): F[Unit] = error(Scope.Root, message, payload, None)
+  final def error(scope: Scope, message: String, payload: => Json): F[Unit] = error(scope, message, payload, None)
+  final def error(message: String, throwable: Throwable): F[Unit] = error(Scope.Root, message, Json.Null, Some(throwable))
+  final def error(scope: Scope, message: String, throwable: Throwable): F[Unit] = error(scope, message, Json.Null, Some(throwable))
+  final def error(message: String, payload: => Json, throwable: Throwable): F[Unit] = error(Scope.Root, message, payload, Some(throwable))
+  final def error(scope: Scope, message: String, payload: => Json, throwable: Throwable): F[Unit] = error(scope, message, payload, Some(throwable))
+
   final def info(
-      scope: Scope = Scope.Root,
-      message: String = "",
-      payload: => Json = Json.Null,
-      throwable: Option[Throwable] = None
+      scope: Scope,
+      message: String,
+      payload: => Json,
+      throwable: Option[Throwable]
   ): F[Unit] = apply(Level.Info, scope, message, payload, throwable)
 
+  final def info(message: String): F[Unit] = info(Scope.Root, message, Json.Null, None)
+  final def info(scope: Scope, message: String): F[Unit] = info(scope, message, Json.Null, None)
+  final def info(payload: => Json): F[Unit] = info(Scope.Root, "", payload, None)
+  final def info(scope: Scope, payload: => Json): F[Unit] = info(scope, "", payload, None)
+  final def info(message: String, payload: => Json): F[Unit] = info(Scope.Root, message, payload, None)
+  final def info(scope: Scope, message: String, payload: => Json): F[Unit] = info(scope, message, payload, None)
+  final def info(message: String, throwable: Throwable): F[Unit] = info(Scope.Root, message, Json.Null, Some(throwable))
+  final def info(scope: Scope, message: String, throwable: Throwable): F[Unit] = info(scope, message, Json.Null, Some(throwable))
+  final def info(message: String, payload: => Json, throwable: Throwable): F[Unit] = info(Scope.Root, message, payload, Some(throwable))
+  final def info(scope: Scope, message: String, payload: => Json, throwable: Throwable): F[Unit] = info(scope, message, payload, Some(throwable))
+
   final def warning(
-      scope: Scope = Scope.Root,
-      message: String = "",
-      payload: => Json = Json.Null,
-      throwable: Option[Throwable] = None
+      scope: Scope,
+      message: String,
+      payload: => Json,
+      throwable: Option[Throwable]
   ): F[Unit] = apply(Level.Warning, scope, message, payload, throwable)
+
+  final def warning(message: String): F[Unit] = warning(Scope.Root, message, Json.Null, None)
+  final def warning(scope: Scope, message: String): F[Unit] = warning(scope, message, Json.Null, None)
+  final def warning(payload: => Json): F[Unit] = warning(Scope.Root, "", payload, None)
+  final def warning(scope: Scope, payload: => Json): F[Unit] = warning(scope, "", payload, None)
+  final def warning(message: String, payload: => Json): F[Unit] = warning(Scope.Root, message, payload, None)
+  final def warning(scope: Scope, message: String, payload: => Json): F[Unit] = warning(scope, message, payload, None)
+  final def warning(message: String, throwable: Throwable): F[Unit] = warning(Scope.Root, message, Json.Null, Some(throwable))
+  final def warning(scope: Scope, message: String, throwable: Throwable): F[Unit] = warning(scope, message, Json.Null, Some(throwable))
+  final def warning(message: String, payload: => Json, throwable: Throwable): F[Unit] = warning(Scope.Root, message, payload, Some(throwable))
+  final def warning(scope: Scope, message: String, payload: => Json, throwable: Throwable): F[Unit] = warning(scope, message, payload, Some(throwable))
 }
 
 object Logger extends Builders[Logger] {
