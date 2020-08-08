7e6477263a09c0a50e3e537a4cd7be79270325fa
Niklas Klein <mail@taig.io>
2020-08-08 11:50:35 +0200

    Add Scope.from(Simple)Name(value: Any) methods

--- modules/core/src/main/scala/io/taig/flog/data/Scope.scala
+++ modules/core/src/main/scala/io/taig/flog/data/Scope.scala
@@ -23,17 +23,25 @@ object Scope {
 
   def of(segments: String*): Scope = Scope(segments.toList)
 
-  def fromName[A: ClassTag]: Scope = {
-    val name = classTag[A].runtimeClass.getName
+  private def fromName(value: Class[_]): Scope = {
+    val name = value.getName
     val normalized = if (name.endsWith("$")) name.init else name
     Scope(normalized.split('.').toList)
   }
 
-  def fromSimpleName[A: ClassTag]: Scope = {
-    val name = classTag[A].runtimeClass.getSimpleName
+  def fromName(value: Any): Scope = fromName(value.getClass)
+
+  def fromName[A: ClassTag]: Scope = fromName(classTag[A].runtimeClass)
+
+  private def fromSimpleName(value: Class[_]): Scope = {
+    val name = value.getSimpleName
     val normalized = if (name.endsWith("$")) name.init else name
     Scope.of(normalized)
   }
+
+  def fromSimpleName(value: Any): Scope = fromSimpleName(value.getClass)
+
+  def fromSimpleName[A: ClassTag]: Scope = fromSimpleName(classTag[A].runtimeClass)
 
   implicit val monoid: Monoid[Scope] = new Monoid[Scope] {
     override def empty: Scope = Root
