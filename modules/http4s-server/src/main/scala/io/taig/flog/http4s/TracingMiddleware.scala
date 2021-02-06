1be4a96111c7628ca2932ca6d61d4cf0cb20b0ee
Niklas Klein <mail@taig.io>
2021-02-06 11:26:47 +0100

    Change TracingMiddleware to use Http, not HttpRoutes

--- modules/http4s-server/src/main/scala/io/taig/flog/http4s/TracingMiddleware.scala
+++ modules/http4s-server/src/main/scala/io/taig/flog/http4s/TracingMiddleware.scala
@@ -5,11 +5,11 @@ import java.util.UUID
 import cats.effect.Sync
 import cats.syntax.all._
 import io.taig.flog.ContextualLogger
-import org.http4s.HttpApp
+import org.http4s.Http
 
 object TracingMiddleware {
-  def apply[F[_]](logger: ContextualLogger[F])(app: HttpApp[F])(implicit F: Sync[F]): HttpApp[F] =
-    HttpApp[F] { request =>
-      F.delay(UUID.randomUUID()).flatMap(uuid => logger.local(app.run(request))(_.trace(uuid)))
+  def apply[F[_], G[_]](logger: ContextualLogger[F])(http: Http[F, G])(implicit F: Sync[F]): Http[F, G] =
+    Http[F, G] { request =>
+      F.delay(UUID.randomUUID()).flatMap(uuid => logger.local(http.run(request))(_.trace(uuid)))
     }
 }
