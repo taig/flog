93190d9ede6087a6d963cca39eed5d583c3e01fe
Niklas Klein <mail@taig.io>
2021-05-23 11:58:32 +0200

    Waiting for circe, then ready to go

--- modules/slf4j/src/main/scala-3/org/slf4j/impl/FlogSlf4jLogger.scala
+++ modules/slf4j/src/main/scala-3/org/slf4j/impl/FlogSlf4jLogger.scala
@@ -6,8 +6,8 @@ import org.slf4j.helpers.{FormattingTuple, MarkerIgnoringBase, MessageFormatter}
 import java.util.Objects
 
 class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) extends MarkerIgnoringBase {
-  def log(level: Level, format: String, args: Array[? <: Object]): Unit =
-    log(level, MessageFormatter.arrayFormat(format, args))
+  def log(level: Level, format: String, args: Array[_ <: AnyRef]): Unit =
+    log(level, MessageFormatter.arrayFormat(format, args.asInstanceOf[Array[AnyRef]]))
 
   def log(level: Level, message: String): Unit = log(level, message, null: Throwable)
 
@@ -30,7 +30,7 @@ class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) ext
   override def trace(format: String, arg1: AnyRef, arg2: AnyRef): Unit =
     log(Level.Debug, format, Array(arg1, arg2))
 
-  override def trace(format: String, arguments: Array[? <: Object]): Unit = log(Level.Debug, format, arguments)
+  override def trace(format: String, arguments: Array[_ <: AnyRef]): Unit = log(Level.Debug, format, arguments)
 
   override def trace(msg: String, t: Throwable): Unit = log(Level.Debug, msg, t)
 
@@ -44,10 +44,7 @@ class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) ext
   override def debug(format: String, arg1: AnyRef, arg2: AnyRef): Unit =
     log(Level.Debug, format, Array(arg1, arg2))
 
-//  override def debug(format: String, arguments: AnyRef*): Unit =
-//    log(Level.Debug, format, arguments.toArray)
-
-  override def debug(format: String, arguments: Array[? <: Object]): Unit = ???
+  override def debug(format: String, arguments: Array[_ <: AnyRef]): Unit = log(Level.Debug, format, arguments)
 
   override def debug(msg: String, t: Throwable): Unit = log(Level.Debug, msg, t)
 
@@ -61,8 +58,7 @@ class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) ext
   override def info(format: String, arg1: AnyRef, arg2: AnyRef): Unit =
     log(Level.Info, format, Array(arg1, arg2))
 
-  override def info(format: String, arguments: Array[? <: Object]): Unit = ???
-//    log(Level.Info, format, arguments.toArray)
+  override def info(format: String, arguments: Array[_ <: AnyRef]): Unit = log(Level.Info, format, arguments)
 
   override def info(msg: String, t: Throwable): Unit = log(Level.Info, msg, t)
 
@@ -76,8 +72,7 @@ class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) ext
   override def warn(format: String, arg1: AnyRef, arg2: AnyRef): Unit =
     log(Level.Warning, format, Array(arg1, arg2))
 
-  override def warn(format: String, arguments: Array[? <: Object]): Unit = ???
-//    log(Level.Warning, format, arguments.toArray)
+  override def warn(format: String, arguments: Array[_ <: AnyRef]): Unit = log(Level.Warning, format, arguments)
 
   override def warn(msg: String, t: Throwable): Unit =
     log(Level.Warning, msg, t)
@@ -92,8 +87,7 @@ class FlogSlf4jLogger(unsafeLog: (Level, String, Option[Throwable]) => Unit) ext
   override def error(format: String, arg1: AnyRef, arg2: AnyRef): Unit =
     log(Level.Error, format, Array(arg1, arg2))
 
-  override def error(format: String, arguments: Array[? <: Object]): Unit = ???
-//    log(Level.Error, format, arguments.toArray)
+  override def error(format: String, arguments: Array[_ <: AnyRef]): Unit = log(Level.Error, format, arguments)
 
   override def error(msg: String, t: Throwable): Unit = log(Level.Error, msg, t)
 }
