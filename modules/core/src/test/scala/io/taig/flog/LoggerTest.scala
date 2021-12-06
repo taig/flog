dab4b8d7dc26d0193cd8e3e4a9107ff675faf53f
Niklas Klein <mail@taig.io>
2021-12-06 21:01:25 +0000

    Fix tests

--- modules/core/src/test/scala/io/taig/flog/LoggerTest.scala
+++ modules/core/src/test/scala/io/taig/flog/LoggerTest.scala
@@ -17,13 +17,13 @@ final class LoggerTest extends CatsEffectSuite {
             l.info("foobar")
         }
       )
-      .map(target => assertIO(obtained = target.get.map(_.length), returns = 1))
+      .flatMap(target => assertIO(obtained = target.get.map(_.length), returns = 1))
   }
 
   test("batched flushes before close") {
     Ref[IO]
       .of(List.empty[Event])
       .flatTap(target => Logger.batched[IO](Logger.list[IO](target), buffer = 5).use(_.info("foobar")))
-      .map(target => assertIO(obtained = target.get.map(_.length), returns = 1))
+      .flatMap(target => assertIO(obtained = target.get.map(_.length), returns = 1))
   }
 }
