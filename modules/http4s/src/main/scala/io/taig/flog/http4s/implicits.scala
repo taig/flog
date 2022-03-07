7f932f8fa9dce9553ded9f95f70f26f93e69f885
Niklas Klein <mail@taig.io>
2022-03-07 14:10:07 +0100

    Fix unused http4s encoders warnings

--- modules/http4s/src/main/scala/io/taig/flog/http4s/implicits.scala
+++ modules/http4s/src/main/scala/io/taig/flog/http4s/implicits.scala
@@ -1,17 +1,13 @@
 package io.taig.flog.http4s
 
 import cats.syntax.all._
-import io.circe.{Encoder, JsonObject}
 import io.circe.syntax._
-import org.http4s.{Headers, Method, Request, Response, Uri}
+import io.circe.{Encoder, JsonObject}
+import org.http4s.{Headers, Request, Response}
 
 object implicits {
-  implicit private val encoderMethod: Encoder[Method] = Encoder[String].contramap(_.show)
-
-  implicit private val encoderUri: Encoder[Uri] = Encoder[String].contramap(_.show)
-
-  implicit private val encoderHeaders: Encoder[Headers] =
-    Encoder[Map[String, String]].contramap(_.headers.map(header => (header.name.show, header.value)).toMap)
+  implicit private val encoderHeaders: Encoder[Headers] = Encoder[Map[String, String]]
+    .contramap(_.headers.map(header => (header.name.show, header.value)).toMap)
 
   implicit def encoderRequest[F[_]]: Encoder.AsObject[Request[F]] = request =>
     JsonObject(
