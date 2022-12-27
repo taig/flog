87bd019649a155f0ff733aeb57753dffa3597e0c
Niklas Klein <mail@taig.io>
2022-12-27 13:00:53 +0100

    Migrate CI/CD to GitHub Actions

---
+++ project/GithubActionsGenerator.scala
@@ -0,0 +1,106 @@
+import io.circe.Json
+import io.circe.syntax._
+
+object GithubActionsGenerator {
+  object Step {
+    def setupJava(version: String): Json = Json.obj(
+      "name" := "Setup Java JDK",
+      "uses" := "actions/setup-java@v3",
+      "with" := Json.obj(
+        "distribution" := "temurin",
+        "java-version" := version,
+        "cache" := "sbt"
+      )
+    )
+
+    val Checkout: Json = Json.obj(
+      "name" := "Checkout",
+      "uses" := "actions/checkout@v3",
+      "with" := Json.obj(
+        "fetch-depth" := 0
+      )
+    )
+  }
+
+  object Job {
+    def lint(javaVersion: String): Json = Json.obj(
+      "name" := "Fatal warnings and code formatting",
+      "runs-on" := "ubuntu-latest",
+      "steps" := List(
+        Step.Checkout,
+        Step.setupJava(javaVersion),
+        Json.obj(
+          "name" := "Workflows",
+          "run" := "sbt -Dmode=ci blowoutCheck"
+        ),
+        Json.obj(
+          "name" := "Code formatting",
+          "run" := "sbt -Dmode=ci scalafmtCheckAll"
+        ),
+        Json.obj(
+          "name" := "Fatal warnings",
+          "run" := "sbt -Dmode=ci +compile"
+        )
+      )
+    )
+
+    def test(javaVersion: String): Json = Json.obj(
+      "name" := "Unit tests",
+      "runs-on" := "ubuntu-latest",
+      "steps" := List(
+        Step.Checkout,
+        Step.setupJava(javaVersion),
+        Json.obj(
+          "name" := "Tests",
+          "run" := "sbt +test"
+        )
+      )
+    )
+  }
+
+  def main(javaVersion: String): Json = Json.obj(
+    "name" := "CI & CD",
+    "on" := Json.obj(
+      "push" := Json.obj(
+        "branches" := List("main"),
+        "tags" := List("*.*.*")
+      )
+    ),
+    "jobs" := Json.obj(
+      "lint" := Job.lint(javaVersion),
+      "test" := Job.test(javaVersion),
+      "deploy" := Json.obj(
+        "name" := "Deploy",
+        "runs-on" := "ubuntu-latest",
+        "needs" := List("test", "lint"),
+        "steps" := List(
+          Step.Checkout,
+          Step.setupJava(javaVersion),
+          Json.obj(
+            "name" := "Release",
+            "run" := "sbt ci-release",
+            "env" := Json.obj(
+              "PGP_PASSPHRASE" := "${{secrets.PGP_PASSPHRASE}}",
+              "PGP_SECRET" := "${{secrets.PGP_SECRET}}",
+              "SONATYPE_PASSWORD" := "${{secrets.SONATYPE_PASSWORD}}",
+              "SONATYPE_USERNAME" := "${{secrets.SONATYPE_USERNAME}}"
+            )
+          )
+        )
+      )
+    )
+  )
+
+  def pullRequest(javaVersion: String): Json = Json.obj(
+    "name" := "CI",
+    "on" := Json.obj(
+      "pull_request" := Json.obj(
+        "branches" := List("main")
+      )
+    ),
+    "jobs" := Json.obj(
+      "lint" := Job.lint(javaVersion),
+      "test" := Job.test(javaVersion)
+    )
+  )
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,6 @@
 addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
+addSbtPlugin("com.github.sbt" % "sbt-ci-release" % "1.5.10")
+addSbtPlugin("io.taig" % "sbt-blowout-yaml-circe" % "0.1.1")
 addSbtPlugin("io.taig" % "sbt-houserules" % "0.6.1")
 addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.2.0")
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.12.0")
